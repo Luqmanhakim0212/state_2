4f1af97693d1edf0346f43c11cb9b80583e87330
Luqman Nurhakim <luqman@besquare.com.my>
2021-11-02 09:37:24 +0800

    exercise 2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Capitalize'),
+        title: const Text('Capitalized'),
       ),
       body: Center(
         child: Padding(
